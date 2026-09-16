a42f76398b2ceae0fc58ac14faea9f0d580840c3
precioufabiyi <preciousfabiyi10@gmail.com>
2026-09-16 08:14:59 +0100

    pp

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,13 @@
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+
+    console.log(this.product);
+  }
+}
