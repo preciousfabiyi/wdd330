984da776a0aec382c94f35ebd791602dbc17cbdf
precioufabiyi <preciousfabiyi10@gmail.com>
2026-09-21 18:04:01 +0100

    Complete W03 individual expand inventory

---
+++ src/js/product-listing.js
@@ -0,0 +1,14 @@
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+loadHeaderFooter();
+
+const category = getParam("category");
+const dataSource = new ProductData();
+const listElement = document.querySelector(".product-list");
+
+const list = new ProductList(category, dataSource, listElement);
+list.init();
+
+document.querySelector(".title").textContent = `Top Products: ${category}`;
