79e56569fea1cdf2132a602731d4da288d7e50b3
precioufabiyi <preciousfabiyi10@gmail.com>
2026-09-21 14:30:15 +0100

    Complete W03 dynamic header and footer

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -12,36 +12,38 @@ export default class ProductList {
     this.renderList(data);
   }
 
-  renderList(list) {
-    renderListWithTemplate(
-      this.productCardTemplate,
-      this.listElement,
-      list,
-    );
-  }
+ renderList(list) {
+  renderListWithTemplate(
+    this.productCardTemplate,
+    this.listElement,
+    list,
+    'afterbegin',
+    true,
+  );
+}
 
   productCardTemplate(product) {
-  return `
-    <li class="product-card">
-      <a href="product_pages/index.html?product=${product.Id}">
-        <img
-          src="${product.Image}"
-          alt="${product.Name}"
-        />
-        <h3 class="card__brand">${product.Brand.Name}</h3>
-        <h2 class="card__name">${product.NameWithoutBrand}</h2>
-        <p class="product-card__price">$${product.FinalPrice}</p>
-        ${
-          product.FinalPrice < product.SuggestedRetailPrice
-            ? `<span class="discount">-${Math.round(
-                ((product.SuggestedRetailPrice - product.FinalPrice) /
-                  product.SuggestedRetailPrice) *
-                  100,
-              )}% OFF</span>`
-            : ''
-        }
-      </a>
-    </li>
-  `;
-}
+    return `
+      <li class="product-card">
+        <a href="product_pages/index.html?product=${product.Id}">
+          <img
+            src="${product.Image}"
+            alt="${product.Name}"
+          />
+          <h3 class="card__brand">${product.Brand.Name}</h3>
+          <h2 class="card__name">${product.NameWithoutBrand}</h2>
+          <p class="product-card__price">$${product.FinalPrice}</p>
+          ${
+            product.FinalPrice < product.SuggestedRetailPrice
+              ? `<span class="discount">-${Math.round(
+                  ((product.SuggestedRetailPrice - product.FinalPrice) /
+                    product.SuggestedRetailPrice) *
+                    100,
+                )}% OFF</span>`
+              : ''
+          }
+        </a>
+      </li>
+    `;
+  }
 }
